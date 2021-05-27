d8ce0f3cbdc29a0e63e49a19487ab4e4936d9901
Wesley Klop <w.klop@inthere.nl>
2021-05-27 10:34:17 +0200

    Add motion library

---
+++ lib/Motion/src/Motion.h
@@ -0,0 +1,26 @@
+#ifndef BALANCINGROBOT_MOTION_H
+#define BALANCINGROBOT_MOTION_H
+
+#include <MPU9250_WE.h>
+
+class Motion {
+private:
+    MPU9250_WE mpu9250;
+
+    void calibrate();
+
+public:
+    explicit Motion(int addr);
+
+    Motion();
+
+    void init();
+
+    float getPitch();
+
+    float getTemperature();
+
+    float getRoll();
+};
+
+#endif //BALANCINGROBOT_MOTION_H
