fefa719cd0531696c065793edb8729e103d97d07
Wesley Klop <w.klop@inthere.nl>
2021-06-29 14:47:46 +0200

    cccc

--- server/static/js/main.mjs
+++ server/static/js/main.mjs
@@ -8,11 +8,11 @@ import {
 import { calculateAngle } from './calibration.mjs'
 import { ShotResult } from './utils.mjs'
 
-const dialog = document.querySelector('#result-dialog')
+const resultDialog = document.querySelector('#result-dialog')
+const resultDialogCheckboxes = Array.from(resultDialog.querySelectorAll('input[type="checkbox"]'))
 const calibrationDialog = document.querySelector('#calibration-dialog')
-const resultForm = document.querySelector('#result-form')
-const checkboxes = Array.from(dialog.querySelectorAll('input[type="checkbox"]'))
 const shootStateButton = document.querySelector('#shoot-state-button')
+const resultForm = document.querySelector('#result-form')
 const commandForm = document.querySelector('#command-form')
 const calibrationForm = document.querySelector('#calibration-form')
 
@@ -42,7 +42,7 @@ calibrationForm.addEventListener('submit', async (evt) => {
     await calibrateRobot(angles)
     calibrationDialog.close()
     await setMagnetState(false)
-    resultForm.showModal()
+    resultDialog.showModal()
 })
 
 commandForm.addEventListener('submit', async (evt) => {
@@ -62,7 +62,7 @@ shootStateButton.addEventListener('click', async () => {
         delete shootStateButton.dataset.active
         shootStateButton.innerText = 'Activeer magneet'
 
-        dialog.showModal()
+        resultDialog.showModal()
     } else {
         shootStateButton.dataset.active = ''
         shootStateButton.innerText = 'Schiet!'
@@ -86,7 +86,7 @@ resultForm.addEventListener('submit', async (evt) => {
 
     await submitResults(result)
 
-    dialog.close()
+    resultDialog.close()
     if (isCalibrating === true) {
         calibrationDialog.showModal()
     }
@@ -100,11 +100,11 @@ const ignoreList = {
     8: ['0', '4'],
 }
 
-checkboxes.forEach((el) => {
+resultDialogCheckboxes.forEach((el) => {
     el.addEventListener('change', () => {
         // If we select the perfect one we want to disable all others, else we disable the perfect button
         const handler = el.checked ? 'setAttribute' : 'removeAttribute'
-        checkboxes
+        resultDialogCheckboxes
             .filter((cb) => ignoreList[el.value].includes(cb.value))
             .forEach((cb) => cb[handler]('disabled', ''))
     })
